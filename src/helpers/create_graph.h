8d59f61bd874131fbf45383c5a61f36bdf47edff
Nikolajpopovic1 <Nikolajpopovic69@gmail.com>
2022-04-27 22:23:06 +0200

    dipi

--- src/helpers/create_graph.h
+++ src/helpers/create_graph.h
@@ -12,18 +12,26 @@ G create_graph(int vertices,std::vector<int> seznam,std::vector<std::vector<std:
     int g_half = seznam.size()/2;
 
     for(int i=1; i<=g_half;i+=1){
+
         g.add_edge(seznam[i-1]-1,seznam[i+g_half-1]-1);
+
     }
 
+
     for(int i=0; i < attr_list.size();i++){
+
       std::vector<std::string> sez = attr_list[i];
+
       for(int j = 0; j < sez.size() ;j++){
+
         g.set_vertex_label(j,sez[j]);
+
       }
-   }
+
+    }
 
     return g;
 
-}  // namespace sics
-
+  }  // namespace sics
+}
 #endif  // SICS_READ_GAL_H_-
